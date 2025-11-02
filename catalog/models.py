b337b49b5598ac957f0986e4dafe694fee8486c1
Victor <victormacedonski@gmail.com>
2025-11-02 20:33:36 +0300

    Add TYPE_CHOISES for Release model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=100, unique= True)
+    bio = models.TextField(blank= True, verbose_name="Биогрфия")
+
+    def __str__(self):
+        return self.name
