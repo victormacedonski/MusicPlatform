fd9677136b9223c85e532e2ce9f5db9451388eed
Victor <victormacedonski@gmail.com>
2025-11-06 17:14:54 +0300

    spelling errors fixed

--- catalog/models.py
+++ catalog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Artist(models.Model):
     name = models.CharField(max_length=100, unique= True)
-    bio = models.TextField(blank= True, verbose_name="Биогрфия")
+    bio = models.TextField(blank= True, verbose_name="Биография")
 
     def __str__(self):
         return self.name
@@ -18,7 +18,7 @@ class Release(models.Model):
         ('single', 'Сингл')
     ]
 
-    title = models.CharField(max_length=200, verbose_name="Назаниие")
+    title = models.CharField(max_length=200, verbose_name="Название")
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE,related_name='releases')
     release_type = models.CharField(max_length=10, choices= TYPE_CHOICES,default='album', verbose_name="Тип")
     cover = models.ImageField(upload_to='covers/', blank=True, null=True, verbose_name="Обложка")
@@ -29,6 +29,6 @@ class Release(models.Model):
         return f"{self.artist.name} - {self.title}"
 
     class Meta:
-        verbose_name = ""
-        verbose_name_plural = ""
+        verbose_name = "Релиз"
+        verbose_name_plural = "Релизы"
         ordering = ['-release_date']
