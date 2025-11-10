19d469d67814ee572400b3d78bf7346efcc2fb3e
Victor <victormacedonski@gmail.com>
2025-11-10 21:07:18 +0300

    Add Views

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Release
 
-# Create your views here.
+def release_list(request):
+    releases = Release.objects.select_related('artist').all()
+    return render(request, 'music/releass_list.html', {'releases': releases})
+
+def lelease_detail(request, pk):
+    release = get_object_or_404(Release.objects.prefetch_related('tracks'), pk=pk)
+    return render(request,'music/release_detail.html', {'release': release})
