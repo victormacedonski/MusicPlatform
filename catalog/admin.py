64b5135bcf52ea1e62406a272729dec447a9a07c
Victor <victormacedonski@gmail.com>
2025-11-10 13:03:38 +0300

    Add Admin models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Artist, Release, Track
 
-# Register your models here.
+class TrackInline(admin.TabularInline):
+    model = Track
+    extra = 1
+
+@admin.register(Release)
+class ReleaseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'artist', 'release_type', 'release_date']
+    list_filter = ['release_type', 'release_date']
+    inlines = [TrackInline]
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
