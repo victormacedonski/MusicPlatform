9953154d9d16afdadb2bef3ae06a8802187aa243
Victor <victormacedonski@gmail.com>
2025-11-13 11:48:09 +0300

    Add release_detail.html

--- catalog/views.py
+++ catalog/views.py
@@ -3,8 +3,8 @@ from .models import Release
 
 def release_list(request):
     releases = Release.objects.select_related('artist').all()
-    return render(request, 'music/release_list.html', {'releases': releases})
+    return render(request, 'catalog/release_list.html', {'releases': releases})
 
 def release_detail(request, pk):
     release = get_object_or_404(Release.objects.prefetch_related('tracks'), pk=pk)
-    return render(request,'music/release_detail.html', {'release': release})
+    return render(request,'catalog/release_detail.html', {'release': release})
